00ee9d1bf59208d931d0c4893d2139102fe9c02a
RowinVA <rowin.amsterdam@gmail.com>
2023-01-13 17:47:02 +0100

    Fixes

--- components/utils/Tooltip/Labels.tsx
+++ components/utils/Tooltip/Labels.tsx
@@ -1,7 +1,1 @@
-// export const discussOnTwitterTooltip = (
-//     <>
-//         The link to the article and the author&apos;s Twitter handle will be automatically added to the tweet.
-//     </>
-// );
-
 export const discussOnTwitterTooltip = "The link to the article and the author's Twitter handle will be automatically added to the tweet.";
